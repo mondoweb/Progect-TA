d0d8d4795fe5cd8f098dcbf1dafbae17d7bc79dc
Tommaso <donnarumma.t@libero.it>
2025-06-04 15:10:34 +0200

    added class Giocatore

---
+++ Progects/progect/scopaNapoletana/Giocatore.java
@@ -0,0 +1,41 @@
+package progect.scopaNapoletana;
+
+import java.util.*;
+
+
+public class Giocatore {
+    private String nome;
+    private List<Carta> mano;
+    private int punteggio;
+
+    public Giocatore(String nome) {
+        this.nome = nome;
+        this.mano = new ArrayList<>();
+        this.punteggio = 0;
+    }
+
+    public void riceviCarta(Carta carta) {
+        mano.add(carta);
+    }
+
+    public List<Carta> getMano() {
+        return mano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPunteggio() {
+        return punteggio;
+    }
+
+    public void aggiungiPunto() {
+        punteggio++;
+    }
+
+    public void resetMano() {
+        mano.clear();
+    }
+}
+
